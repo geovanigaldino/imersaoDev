35f0dfa955349381501f95fc8592e1897670fb5c
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2025-04-03 16:08:28 -0300

    atualização

--- aula3/script.js
+++ aula3/script.js
@@ -1,39 +1,40 @@
-let rodada = 1;
-let vidas = 3;
-let venceu = false;
+function jogar() {
+    let rodada = 1;
+    let vidas = 3;
+    let venceu = false;
 
-while (rodada <= 3) {
-    console.log(`Rodada: ${rodada}`);
-    
-    let escolhaJogador = prompt(`Nível ${rodada} escolha vidro (1, 2, 3)?`);
+    while (rodada <= 3) {
+        console.log("Rodada: " + rodada);
 
-    // Verifica se a escolha do jogador é 1, 2 ou 3
-    while (![1, 2, 3].includes(escolhaJogador)) {
-        alert("Escolha inválida! Escolha 1, 2 ou 3.");
-        escolhaJogador = parseInt(prompt(`Nível ${rodada} escolha vidro (1, 2, 3)?`));
-    }
+        let escolhaJogador = parseInt(prompt(`Jogada ${rodada}: Escolha um vidro (1, 2, 3)?`));
 
-    let pisoQuebrado = Math.floor(Math.random() * 3) + 1;
-
-    
-    if (escolhaJogador == pisoQuebrado) {
-        vidas--;
-
-        if (vidas == 0) {
-            alert("Você perdeu!");
-            break;
+        // Garante que o jogador só pode escolher 1, 2 ou 3
+        while (![1, 2, 3].includes(escolhaJogador)) {
+            alert("Escolha inválida! Escolha 1, 2 ou 3.");
+            escolhaJogador = parseInt(prompt(`Jogada ${rodada}: Escolha um vidro (1, 2, 3)?`));
         }
-        
-        alert(`Você perdeu uma vida! Vidas restantes: ${vidas}`);
-    } else {
-        alert(`Passou! Piso quebrado estava na ponte ${pisoQuebrado}`);
+
+        let pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+
+        if (escolhaJogador === pisoQuebrado) {
+            vidas--;
+
+            if (vidas === 0) {
+                alert("Você perdeu!");
+                break; 
+            }
+
+            alert(`Você perdeu uma vida!\nVidas restantes: ${vidas}`);
+        } else {
+            alert(`Passou!\nPiso quebrado estava na ponte ${pisoQuebrado}`);
+        }
+
+        rodada++;
     }
 
-    rodada++;
-}
-
-// Se o jogador chegou até a 4ª rodada e ainda tem vidas, ele venceu
-if (rodada > 3 && vidas > 0) {
-    alert("Parabéns, você venceu!");
-    venceu = true;
+    // Se o jogador chegou até a 4ª rodada e ainda tem vidas, ele venceu
+    if (rodada > 3 && vidas > 0) {
+        venceu = true;
+        alert("Parabéns, você venceu!");
+    }
 }
